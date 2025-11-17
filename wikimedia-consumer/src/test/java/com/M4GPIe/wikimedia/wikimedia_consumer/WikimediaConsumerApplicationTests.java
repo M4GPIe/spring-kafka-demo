98367dcb1fcc351dd71155b6cb6956dc711b5745
Manu <manuel.delossantos@dialapplet.com>
2025-11-17 12:53:45 +0100

    feat: created wikimedia project

---
+++ wikimedia-consumer/src/test/java/com/M4GPIe/wikimedia/wikimedia_consumer/WikimediaConsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.M4GPIe.wikimedia.wikimedia_consumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WikimediaConsumerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
